8657b1698039fdc6e091c398d24b7c0d66f67158
Andrew Chou <andrewchou@fastmail.com>
2026-04-23 11:14:35 +0100

    upgrade deps and configure eslint

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+import { createRequire } from "node:module";
+import globals from "globals";
+
+const require = createRequire(import.meta.url);
+const expo = require(
+  // @ts-expect-error Not worth fixing
+  "expo-module-scripts/eslint.config.base",
+);
+
+export default defineConfig([
+  {
+    name: "ignores",
+    ignores: ["android", "build", "ios"],
+  },
+  js.configs.recommended,
+  tseslint.configs.recommended,
+  {
+    name: "backend",
+    files: ["backend/**/*"],
+    languageOptions: {
+      globals: { ...globals.node, ...globals.nodeBuiltin },
+    },
+  },
+  {
+    name: "expo",
+    extends: [expo],
+    rules: {
+      "import/order": "off",
+      "prettier/prettier": "off",
+    },
+  },
+]);
