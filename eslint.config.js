66a55c2a9447ef7c27d37a1263abba14b3fbb13f
Andrew Chou <andrewchou@fastmail.com>
2026-04-29 12:31:28 +0100

    update and fix eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,40 @@
+// @ts-check
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { includeIgnoreFile } from "@eslint/compat";
 import js from "@eslint/js";
-import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
-import { createRequire } from "node:module";
 import globals from "globals";
+import { createRequire } from "node:module";
+import tseslint from "typescript-eslint";
 
 const require = createRequire(import.meta.url);
 const expo = require("expo-module-scripts/eslint.config.base");
 
+const gitignorePath = path.join(
+  path.dirname(fileURLToPath(import.meta.url)),
+  ".gitignore",
+);
+
+const gitExcludePath = path.join(
+  path.dirname(fileURLToPath(import.meta.url)),
+  ".git",
+  "info",
+  "exclude",
+);
+
 export default defineConfig([
+  includeIgnoreFile(gitignorePath),
+  includeIgnoreFile(gitExcludePath),
   {
     name: "ignores",
-    ignores: ["android", "example", "ios"],
+    ignores: ["android/**/*", "example/**/*", "ios/**/*"],
   },
   js.configs.recommended,
   tseslint.configs.recommended,
   {
     name: "node",
-    files: ["backend", "scripts"],
+    files: ["backend/**/*", "scripts/**/*"],
     languageOptions: {
       globals: { ...globals.node, ...globals.nodeBuiltin },
     },
