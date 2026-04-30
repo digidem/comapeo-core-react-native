7584ef34ac761a044b23c370d48e107b1ebfd4db
Andrew Chou <andrewchou@fastmail.com>
2026-04-30 11:21:00 +0100

    fix SafeAreaView usage

--- apps/testing/src/App.tsx
+++ apps/testing/src/App.tsx
@@ -1,20 +1,22 @@
 import { ScrollView, Text, View } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
 
 import { TestRunner } from './TestRunner'
 
 export default function App() {
 	return (
-		<SafeAreaView style={{ flex: 1 }}>
-			<ScrollView style={{ backgroundColor: 'white' }}>
-				<View style={{ padding: 20 }}>
-					<Text style={{ fontWeight: 'bold', textAlign: 'center' }}>
-						CoMapeo Core React Native Testing App
-					</Text>
-				</View>
+		<SafeAreaProvider>
+			<SafeAreaView style={{ flex: 1 }}>
+				<ScrollView style={{ backgroundColor: 'white' }}>
+					<View style={{ padding: 20 }}>
+						<Text style={{ fontWeight: 'bold', textAlign: 'center' }}>
+							CoMapeo Core React Native Testing App
+						</Text>
+					</View>
 
-				<TestRunner />
-			</ScrollView>
-		</SafeAreaView>
+					<TestRunner />
+				</ScrollView>
+			</SafeAreaView>
+		</SafeAreaProvider>
 	)
 }
