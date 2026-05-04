0b227a34bc46b02e37e8da47ade123544d9804a6
Andrew Chou <andrewchou@fastmail.com>
2026-05-04 12:45:56 +0100

    rename directory and app
    uses "e2e" instead of "testing"

--- apps/e2e/src/App.tsx
+++ apps/e2e/src/App.tsx
@@ -10,7 +10,7 @@ export default function App() {
 				<ScrollView style={{ backgroundColor: 'white' }}>
 					<View style={{ padding: 20 }}>
 						<Text style={{ fontWeight: 'bold', textAlign: 'center' }}>
-							CoMapeo Core React Native Testing App
+							CoMapeo Core React Native E2E App
 						</Text>
 					</View>
 
