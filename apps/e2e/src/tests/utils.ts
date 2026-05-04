381e3461f4c5e38ccc97d9a498b85f6993cc7da1
Andrew Chou <andrewchou@fastmail.com>
2026-05-04 15:01:50 +0100

    chore: add e2e testing app (#49)
    Adds a testing app to `applications/e2e` that can be used to run
    on-device e2e tests. Uses an identical approach to [Expo's test suite
    for their native
    modules](https://github.com/expo/expo/tree/main/apps/test-suite) by
    running
    [`jasmine-core@5`](https://jasmine.github.io/api/5.13/module-jasmine-core.html)
    in the application.
    
    Introduced tests are non-exhaustive. Have currently only ported the
    [project CRUD tests from
    core](https://github.com/digidem/comapeo-core/blob/14afa092d096a9aa024f81beda7a84096d247c81/test-e2e/project-crud.js),
    some of which are failing (seemingly due to legitimate reasons)
    
    ---------
    
    Co-authored-by: Gregor MacLennan <gmaclennan@awana.digital>

---
+++ apps/e2e/src/tests/utils.ts
@@ -0,0 +1,51 @@
+import { ComapeoDoc } from '@comapeo/core/schema.js'
+import { JasmineInterface } from 'jasmine-core/lib/jasmine-core/jasmine'
+
+export type TestContext = Pick<
+	JasmineInterface,
+	'describe' | 'it' | 'expect' | 'expectAsync' | 'jasmine'
+>
+
+export function sortBy<T>(arr: Array<T>, key: keyof T) {
+	return arr.sort(function (a, b) {
+		if (a[key] < b[key]) return -1
+		if (a[key] > b[key]) return 1
+		return 0
+	})
+}
+
+export function sortById(docs: Array<ComapeoDoc>) {
+	return sortBy(docs, 'docId')
+}
+
+export function randomDate() {
+	return new Date(randomNum({ min: 0, max: Date.now() }))
+}
+
+export function randomBool() {
+	return Math.random() >= 0.5
+}
+
+export function randomNum({
+	min = 0,
+	max = 1,
+	precision,
+}: { min?: number; max?: number; precision?: number } = {}) {
+	const num = Math.random() * (max - min) + min
+	if (typeof precision === 'undefined') return num
+	return round(num, precision)
+}
+
+export function round(value: number, decimalPlaces: number) {
+	return Math.round(value * 10 ** decimalPlaces) / 10 ** decimalPlaces
+}
+
+export async function delay(duration: number) {
+	await new Promise((res) => {
+		globalThis.setTimeout(res, duration)
+	})
+}
+
+export function removeUndefinedFields(object: unknown) {
+	return JSON.parse(JSON.stringify(object))
+}
